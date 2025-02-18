d1b6ab3d5333389c44c332d1e8f57fcade85ed6e
Graham Norris <graham.norris@nov.com>
2025-02-18 10:55:46 -0600

    tester

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    backendUrl: 'http://localhost:5000',
+    auth: 'http://localhost:5000/api/auth',
+    user: 'http://localhost:5000/api/users',
+    review: 'http://localhost:5000/api/reviews',
+    search: 'http://localhost:5000/api/search'
+  };
+  // replace when deploying
---
+++ src/environments/environments.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    backendUrl: 'http://localhost:5000',
+    auth: 'http://localhost:5000/api/auth',
+    user: 'http://localhost:5000/api/users',
+    review: 'http://localhost:5000/api/reviews',
+    search: 'http://localhost:5000/api/searches'
+  };
