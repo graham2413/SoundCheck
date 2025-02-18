d3cf60be8bb2c107577ace54ccaff5a3f4735cbb
Graham Norris <graham.norris@nov.com>
2025-02-18 10:57:55 -0600

    Remove environment file from tracking

--- src/environments/environments.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    backendUrl: 'http://localhost:5000',
-    auth: 'http://localhost:5000/api/auth',
-    user: 'http://localhost:5000/api/users',
-    review: 'http://localhost:5000/api/reviews',
-    search: 'http://localhost:5000/api/searches'
-  };
